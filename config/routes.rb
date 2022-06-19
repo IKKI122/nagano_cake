b9b270d7f39862c1db2964636badc1632aa7a309
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-06-19 07:46:48 +0000

    [Add]adressesのコントローラ・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :public do
-    get 'cart_items/index'
+    get 'adresses/index'
+    get 'adresses/edit'
   end
-  
   namespace :admin do
     root to: 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
     resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
+    resources :items, only: [:index, :edit, :create, :update, :destroy]
   end
   
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
