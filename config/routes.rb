0a2ae2ef47c0871b63ce5b11cda25b91a091f371
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-06-19 06:01:24 +0000

    [Add]cart_itemのコントローラ・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/unsubscribe'
+    get 'cart_items/index'
   end
+  
   namespace :admin do
     root to: 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
@@ -15,6 +14,7 @@ Rails.application.routes.draw do
     get "/about"=>"homes#about", as:"about"
     resources :items, only: [:index, :show]
     resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
+    resources :cart_items, only: [:index, :update, :destroy, :destroy_all, :create]
   end
   
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
