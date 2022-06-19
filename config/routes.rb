84b471a06d077cb0361e66a289ac947d3e99343a
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-06-19 05:28:25 +0000

    [Add]customersのコントローラ・ビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,20 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/unsubscribe'
+  end
   namespace :admin do
     root to: 'homes#top'
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
   end
   
   namespace :public do
     root to: 'homes#top'
     get "/about"=>"homes#about", as:"about"
-    get 'items/index'
-    get 'items/show'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
   end
   
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
