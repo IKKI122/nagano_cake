1fecc18ca69d2f403df6ff38b9d825af4ab8a260
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-07-18 07:30:11 +0000

    [Add]注文情報入力画面作成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,7 @@
 class Public::CartItemsController < ApplicationController
   def index
     @cart_item=CartItem.new
-    @cart_items=CartItem.all
+    @cart_items=current_customer.cart_items
     @item=@cart_item.item
     @total=0
   end
@@ -26,12 +26,20 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item=CartItem.new(cart_item_params)
     @cart_item.customer_id=current_customer.id
-    @cart_item.save
-    redirect_to cart_items_path
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+    	cart_item=current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    	cart_item.amount+=params[:cart_item][:amount].to_i
+    	cart_item.save
+    	redirect_to cart_items_path
+		elsif @cart_item.save
+		  redirect_to cart_items_path
+		else
+		  render :show
+		end
   end
   
   private
   def cart_item_params
-      params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+      params.require(:cart_item).permit(:item_id, :amount)
   end
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,31 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order=Order.new
   end
   
   def confirm
+    @cart_items=current_customer.cart_items.all
+    @order=current_customer.orders.new
+    @order.payment_method=params[:order][:payment_method]
+    if params[:order][:address_id]=="0"
+      @order.postal_code=current_customer.postal_code
+      @order.address=current_customer.address
+      @order.name=current_customer.last_name+current_customer.first_name
+    elsif params[:order][:address_id]=="1"
+      @order.postal_code=Address.find(params[:order][:select_address]).postal_code
+      @order.address=Address.find(params[:order][:select_address]).address
+      @order.name=Address.find(params[:order][:select_address]).name
+    elsif params[:order][:address_id]=="2"
+      @order.postal_code=params[:order][:postal_code]
+      @order.address=params[:order][:address]
+      @order.name=params[:order][:name]
+    else
+      render :new
+    end
   end
   
   def create
+   
   end
 
   def complete
@@ -15,5 +35,14 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
+  end
+  
+  private
+  def order_params
+      params.require(:order).permit(:postal_code, :address, :name, :payment_method)
+  end
+  
+  def address_params
+      params.require(:order).permit(:postal_code, :address, :name)
   end
 end
