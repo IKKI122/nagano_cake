02929bc7fdb77f22308a6f88b9b7a65f79783472
IKKI122 <100700431+IKKI122@users.noreply.github.com>
2022-07-31 14:32:38 +0000

    [Add]管理者側注文履歴詳細画面作成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,5 +2,19 @@ class Admin::OrderDetailsController < ApplicationController
     def update
         @order_detail=OrderDetail.find(params[:id])
         @order=@order_detail.order
+        @order_detail.update(order_detail_params)
+        if @order_detail.making_status=='working'
+            @order.update(status: 'working')
+        elsif @order.order_details.count==@order.order_details.where(making_status: 'completed').count
+			@order.update(status: 'preparing')
+        end
+        redirect_to admin_order_path(@order.id)
+        
+        
+    end
+    
+    private
+    def order_detail_params
+        params.require(:order_detail).permit(:making_status)
     end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,8 +4,22 @@ class Admin::OrdersController < ApplicationController
   end
   
   def show
+    @order=Order.find(params[:id])
+    @order_details=@order.order_details
   end
   
   def update
+    @order=Order.find(params[:id])
+    @order_details=@order.order_details
+    @order.update(order_params)
+    if @order.status=='paid_up' 
+	     @order_details.update_all(making_status: 'waiting') 
+    end
+    redirect_to admin_order_path(@order.id)
+  end
+  
+  private
+  def order_params
+      params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_payment, :status, :customer_id)
   end
 end
